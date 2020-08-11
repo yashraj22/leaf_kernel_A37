Rashed97/android_kernel_oppo_msm8939

input: synaptics: Send KEY_WAKEUP for double-tap event
KEY_POWER will confuse the system to end call.
If press power button to end call is enabled.
Use KEY_WAKEUP instead

Change-Id: I22a5fb67ce4af960ffaf911846c10e534d787a86

drivers/input/touchscreen/synaptics_oppo/synaptics_oppo_driver_3508.c
@@ -1168,7 +1168,7 @@ static void gesture_judge(struct synaptics_ts_data *ts)
 		case DTAP_DETECT:
 			gesture = DouTap;
 			if (ts->double_enable)
-				keycode = KEY_POWER;
+				keycode = KEY_WAKEUP;
 			break;
 		case SWIPE_DETECT:
 			gesture =   (regswipe == 0x41) ? Left2RightSwip   :
@@ -2360,7 +2360,7 @@ static int	synaptics_input_init(struct synaptics_ts_data *ts)
 	set_bit(INPUT_PROP_DIRECT, ts->input_dev->propbit);
 
 #ifdef SUPPORT_GESTURE
-	set_bit(KEY_POWER , ts->input_dev->keybit);//doulbe-tap resume
+	set_bit(KEY_WAKEUP , ts->input_dev->keybit);//doulbe-tap resume
 	set_bit(KEY_GESTURE_CIRCLE, ts->input_dev->keybit);
 	set_bit(KEY_GESTURE_SWIPE_DOWN, ts->input_dev->keybit);
 	set_bit(KEY_GESTURE_V, ts->input_dev->keybit);

yashraj22/leaf_kernel_A37

drivers/input/touchscreen/synaptics_oppo/synaptics_oppo_driver_3508.c
@@ -1115,7 +1115,7 @@ static void gesture_judge(struct synaptics_ts_data *ts)
 		case DTAP_DETECT:
 			gesture = DouTap;
 			if (ts->double_enable)
-				keycode = KEY_POWER;
+				keycode = KEY_WAKEUP;
 			break;
 		case SWIPE_DETECT:
 			gesture =   (regswipe == 0x41) ? Left2RightSwip   :
@@ -2306,7 +2306,7 @@ static int	synaptics_input_init(struct synaptics_ts_data *ts)
 	set_bit(INPUT_PROP_DIRECT, ts->input_dev->propbit);
 
 #ifdef SUPPORT_GESTURE
-	set_bit(KEY_POWER, ts->input_dev->keybit); //double-tap resume
+	set_bit(KEY_WAKEUP, ts->input_dev->keybit); //double-tap resume
 	set_bit(KEY_GESTURE_CIRCLE, ts->input_dev->keybit);
 	set_bit(KEY_GESTURE_SWIPE_DOWN, ts->input_dev->keybit);
 	set_bit(KEY_GESTURE_V, ts->input_dev->keybit);